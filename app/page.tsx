f59bef31cee0ee902c3fba8ce75514cb81ae0229
joshkotrous <joshkotrous@gmail.com>
2026-01-31 12:10:52 -0500

    added intensity controls

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,12 @@ import ParticleCloud from "./components/shaders/ParticleCloud";
 import { ThemeProvider } from "./components/providers/themeProvider";
 import SineWaveGrid from "./components/shaders/SineWaveGrid";
 import { AsciiAudioEffect } from "./components/shaders/AsciiAudioEffect";
+import MagiCore from "./components/shaders/MagiCore";
+import RadarSweep from "./components/shaders/RadarSweep";
+import NeuralWeb from "./components/shaders/NeuralWeb";
+import DNAHelix from "./components/shaders/DNAHelix";
 import { ThemeDropdown } from "./components/ThemeDropdown";
+
 export default function Home() {
   return (
     <div className="flex h-screen flex-col overflow-hidden">
@@ -25,9 +30,21 @@ export default function Home() {
           <div className="w-[280px] h-[200px] overflow-visible">
             <SineWaveGrid />
           </div>
+          <div className="w-[280px] h-[200px] overflow-visible">
+            <DNAHelix />
+          </div>
         </div>
 
         <div className="flex justify-center items-center gap-8 p-4 overflow-visible">
+          <div className="w-[280px] h-[200px] overflow-visible">
+            <MagiCore />
+          </div>
+          <div className="w-[280px] h-[200px] overflow-visible">
+            <RadarSweep />
+          </div>
+          <div className="w-[280px] h-[200px] overflow-visible">
+            <NeuralWeb />
+          </div>
           <div className="w-[280px] h-[200px] overflow-visible">
             <AsciiAudioEffect />
           </div>
